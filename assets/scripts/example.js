24008e5c60209f6f8a0d2b9c19e77d29756823b1
Celena T <chibiynm@gmail.com>
2016-04-23 17:31:22 -0400

    Add donations summary

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,7 +1,33 @@
 'use strict';
 
-// $('.donate-items').children().on('click', function(){
-//   $(this).toggleClass("active");
-// });
+$('.water-add').on('click', function() {
+  $('.donation-water').replaceWith(function(){
+    return '$' + $('.water-amount').val();
+  });
+});
 
-module.exports = true;
+$('.food-add').on('click', function() {
+  $('.donation-food').replaceWith(function(){
+    return '$' + $('.food-amount').val();
+  });
+});
+
+$('.medical-add').on('click', function() {
+  $('.donation-medical').replaceWith(function(){
+    return '$' + $('.medical-amount').val();
+  });
+});
+
+$('.money-add').on('click', function() {
+  $('.donation-money').replaceWith(function(){
+    return '$' + $('.money-amount').val();
+  });
+});
+
+$('.donation-total').replaceWith(function() {
+  return $('.water-amount').val() + $('.food-amount').val() + $('.medical-amount').val() + $('.money-amount').val();
+});
+
+$(document).ready(() => {
+
+});
