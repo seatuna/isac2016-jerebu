6dfeb140b7aace46900eb724dabbc16b28e97444
Celena T <chibiynm@gmail.com>
2016-04-23 18:07:26 -0400

    Add donation total

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,31 +1,38 @@
 'use strict';
 
+let total = function() {
+  $('.donation-total').html(function() {
+    let total = Number($('.water-amount').val()) + Number($('.food-amount').val()) + Number($('.medical-amount').val()) + Number($('.money-amount').val());
+    return "$" + total;
+  });
+};
+
 $('.water-add').on('click', function() {
   $('.donation-water').replaceWith(function(){
+    total();
     return '$' + $('.water-amount').val();
   });
 });
 
 $('.food-add').on('click', function() {
   $('.donation-food').replaceWith(function(){
+    total();
     return '$' + $('.food-amount').val();
   });
 });
 
 $('.medical-add').on('click', function() {
   $('.donation-medical').replaceWith(function(){
+    total();
     return '$' + $('.medical-amount').val();
   });
 });
 
 $('.money-add').on('click', function() {
   $('.donation-money').replaceWith(function(){
+    total();
     return '$' + $('.money-amount').val();
   });
-});
-
-$('.donation-total').replaceWith(function() {
-  return $('.water-amount').val() + $('.food-amount').val() + $('.medical-amount').val() + $('.money-amount').val();
 });
 
 $(document).ready(() => {
