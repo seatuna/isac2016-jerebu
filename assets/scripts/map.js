79f3521629ced7a60df214afd76d951c609f6099
Celena T <chibiynm@gmail.com>
2016-04-24 11:23:57 -0400

    Add google map heatlayer

---
+++ assets/scripts/map.js
@@ -0,0 +1,65 @@
+'use strict';
+
+let map, heatmap;
+
+function initMap() {
+  map = new google.maps.Map($("#map")[0], {
+    zoom: 4,
+    center: {lat: -2.2180, lng: 115.6628},
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  });
+
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: getPoints(),
+    map: map
+  });
+}
+
+function toggleHeatmap() {
+  heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+function changeGradient() {
+  let gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+  ];
+  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
+
+function changeRadius() {
+  heatmap.set('radius', heatmap.get('radius') ? null : 20);
+}
+
+function changeOpacity() {
+  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+}
+
+// Heatmap data: 500 Points
+function getPoints() {
+  return [
+    new google.maps.LatLng(27.24, 140.886),
+    new google.maps.LatLng(18.369, 122.276),
+    new google.maps.LatLng(18.37, 122.282)
+  ];
+}
+
+$(document).ready(() => {
+  initMap();
+  // toggleHeatmap();
+  // changeRadius();
+  // changeOpacity();
+  // changeGradient();
+});
